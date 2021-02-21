65ff360382a423b33b5af181c17749b504aae52e
lawyerlxc <285452292@qq.com>
2021-02-21 23:35:21 +0800

    第七次

--- gulpfile-dev.js
+++ gulpfile-dev.js
@@ -18,31 +18,104 @@ task('html',async ()=>{
 // 处理css
 task('style',async ()=>{
   src('./style/*.css')
-  .pipe(dest('./dist/style'))
+  .pipe(dest('./dist/css'))
   .pipe(load.connect.reload())
 })
 
 // 编译sass
 task('sass',async ()=>{
-  src('./style/*.scss')
+  src('./sass/*.scss')
   .pipe(load.sass().on('error', load.sass.logError))//编译sass
-  .pipe(dest('./dist/style'))
+  .pipe(dest('./dist/sass'))
   .pipe(load.connect.reload())
 })
 
 // 处理js
 task('script',async ()=>{
   src('./script/*.js')
-  .pipe(dest('./dist/script'))
+  .pipe(dest('./dist/js'))
+  .pipe(load.connect.reload())
+})
+task('script2',async ()=>{
+  src('./script/app/*.js')
+  .pipe(dest('./dist/js/app'))
+  .pipe(load.connect.reload())
+})
+task('script3',async ()=>{
+  src('./script/lib/*.js')
+  .pipe(dest('./dist/js/lib'))
   .pipe(load.connect.reload())
 })
 
 // 处理img
 task('image',async ()=>{
   src('./images/*.*')
-  .pipe(dest('./dist/images'))
+  .pipe(dest('./dist/img'))
   .pipe(load.connect.reload())
 })
+task('image2',async ()=>{
+  src('./images/index_carousel/*.*')
+  .pipe(dest('./dist/img/index_carousel'))
+  .pipe(load.connect.reload())
+})
+task('image3',async ()=>{
+  src('./images/index_goods_carousel/*.*')
+  .pipe(dest('./dist/img/index_goods_carousel'))
+  .pipe(load.connect.reload())
+})
+task('image4',async ()=>{
+  src('./images/Logintoregister/*.*')
+  .pipe(dest('./dist/img/Logintoregister'))
+  .pipe(load.connect.reload())
+})
+task('image41',async ()=>{
+  src('./images/Logintoregister/V_code/*.*')
+  .pipe(dest('./dist/img/Logintoregister/V_code'))
+  .pipe(load.connect.reload())
+})
+task('image5',async ()=>{
+  src('./images/magnify/*.*')
+  .pipe(dest('./dist/img/magnify'))
+  .pipe(load.connect.reload())
+})
+task('image6',async ()=>{
+  src('./images/other-infos/*.*')
+  .pipe(dest('./dist/img/other-infos'))
+  .pipe(load.connect.reload())
+})
+task('image7',async ()=>{
+  src('./images/public_paging_header/*.*')
+  .pipe(dest('./dist/img/public_paging_header'))
+  .pipe(load.connect.reload())
+})
+task('image8',async ()=>{
+  src('./images/share/*.*')
+  .pipe(dest('./dist/img/share'))
+  .pipe(load.connect.reload())
+})
+//处理 json
+task('json',async ()=>{
+  src('./json/*.*')
+  .pipe(dest('./dist/json'))
+  .pipe(load.connect.reload())
+})
+task('json2',async ()=>{
+  src('./json/other/*.*')
+  .pipe(dest('./dist/json/other'))
+  .pipe(load.connect.reload())
+})
+task('json3',async ()=>{
+  src('./json/product list/*.*')
+  .pipe(dest('./dist/json/product list'))
+  .pipe(load.connect.reload())
+})
+
+task('config',async ()=>{
+  src('./config.js')
+  .pipe(dest('./dist'))
+  .pipe(load.connect.reload())
+})
+
 
 // 启动一个服务，实现自动刷新
 task('reload',async ()=>{
@@ -55,13 +128,14 @@ task('reload',async ()=>{
 // 监听文件变化
 task('watch',async ()=>{
   watch('./views/*.html',series('html'))
-  watch('./style/*.scss',series('sass'))
-  watch('./script/*.js',series('script'))
-  watch('./images/*.*',series('image'))
+  watch('./style/*',series('sass'))
+  watch('./script/*',series('script'))
+  watch('./images/*',series('image'))
+  // watch('./json/*',series('json'))
 })
 
 // 打包（开发环境）
-task('dev',series('delDist','html','sass','style','script','image'))
+task('dev',series('delDist','html','sass','style','script','script2','script3','image','image2','image3','image4','image5','image6','image7','image8','image41','json','json2','json3','config'))
 
 // 启动项目
 task('start',series('dev','reload','watch'))
