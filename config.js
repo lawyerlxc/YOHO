a37d1a6977250215402589fe9f0de52c6af64312
lawyerlxc <285452292@qq.com>
2021-02-23 20:54:02 +0800

    第8次

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/3/12.
  */
 requirejs.config({
-    baseUrl:"./js/app",
+    baseUrl:"../js/app",
     paths:{
         "jquery":"../lib/jquery-1.11.3",
         "jquery-ui":"http://code.jquery.com/ui/1.10.4/jquery-ui",
--- gulpfile-dev.js
+++ gulpfile-dev.js
@@ -10,14 +10,14 @@ task('delDist',async ()=>{
 
 // 处理html
 task('html',async ()=>{
-  src('./views/*.html')
-  .pipe(dest('./dist'))
+  src('./html/*.html')
+  .pipe(dest('./dist/html'))
   .pipe(load.connect.reload())
 })
 
 // 处理css
 task('style',async ()=>{
-  src('./style/*.css')
+  src('./css/**/*')
   .pipe(dest('./dist/css'))
   .pipe(load.connect.reload())
 })
@@ -26,89 +26,32 @@ task('style',async ()=>{
 task('sass',async ()=>{
   src('./sass/*.scss')
   .pipe(load.sass().on('error', load.sass.logError))//编译sass
-  .pipe(dest('./dist/sass'))
+  .pipe(dest('./dist/css'))
   .pipe(load.connect.reload())
 })
 
 // 处理js
 task('script',async ()=>{
-  src('./script/*.js')
+  src('./js/**/*')
   .pipe(dest('./dist/js'))
   .pipe(load.connect.reload())
 })
-task('script2',async ()=>{
-  src('./script/app/*.js')
-  .pipe(dest('./dist/js/app'))
-  .pipe(load.connect.reload())
-})
-task('script3',async ()=>{
-  src('./script/lib/*.js')
-  .pipe(dest('./dist/js/lib'))
-  .pipe(load.connect.reload())
-})
+
 
 // 处理img
 task('image',async ()=>{
-  src('./images/*.*')
+  src('./img/**/*')
   .pipe(dest('./dist/img'))
   .pipe(load.connect.reload())
 })
-task('image2',async ()=>{
-  src('./images/index_carousel/*.*')
-  .pipe(dest('./dist/img/index_carousel'))
-  .pipe(load.connect.reload())
-})
-task('image3',async ()=>{
-  src('./images/index_goods_carousel/*.*')
-  .pipe(dest('./dist/img/index_goods_carousel'))
-  .pipe(load.connect.reload())
-})
-task('image4',async ()=>{
-  src('./images/Logintoregister/*.*')
-  .pipe(dest('./dist/img/Logintoregister'))
-  .pipe(load.connect.reload())
-})
-task('image41',async ()=>{
-  src('./images/Logintoregister/V_code/*.*')
-  .pipe(dest('./dist/img/Logintoregister/V_code'))
-  .pipe(load.connect.reload())
-})
-task('image5',async ()=>{
-  src('./images/magnify/*.*')
-  .pipe(dest('./dist/img/magnify'))
-  .pipe(load.connect.reload())
-})
-task('image6',async ()=>{
-  src('./images/other-infos/*.*')
-  .pipe(dest('./dist/img/other-infos'))
-  .pipe(load.connect.reload())
-})
-task('image7',async ()=>{
-  src('./images/public_paging_header/*.*')
-  .pipe(dest('./dist/img/public_paging_header'))
-  .pipe(load.connect.reload())
-})
-task('image8',async ()=>{
-  src('./images/share/*.*')
-  .pipe(dest('./dist/img/share'))
-  .pipe(load.connect.reload())
-})
+
 //处理 json
 task('json',async ()=>{
-  src('./json/*.*')
+  src('./json/**/*')
   .pipe(dest('./dist/json'))
   .pipe(load.connect.reload())
 })
-task('json2',async ()=>{
-  src('./json/other/*.*')
-  .pipe(dest('./dist/json/other'))
-  .pipe(load.connect.reload())
-})
-task('json3',async ()=>{
-  src('./json/product list/*.*')
-  .pipe(dest('./dist/json/product list'))
-  .pipe(load.connect.reload())
-})
+
 
 task('config',async ()=>{
   src('./config.js')
@@ -135,7 +78,7 @@ task('watch',async ()=>{
 })
 
 // 打包（开发环境）
-task('dev',series('delDist','html','sass','style','script','script2','script3','image','image2','image3','image4','image5','image6','image7','image8','image41','json','json2','json3','config'))
+task('dev',series('delDist','html','sass','style','script','image','json','config'))
 
 // 启动项目
 task('start',series('dev','reload','watch'))
